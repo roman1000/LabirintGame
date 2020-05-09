1031445c00843ac20518822cd64c9d55a4178a63
Roman <roman2005.19.07@gmail.com>
2020-05-09 20:50:18 +0300

    gamecommit

--- LabirintGame/Program.cs
+++ LabirintGame/Program.cs
@@ -8,50 +8,74 @@ namespace LabirintGame
         static void Main()
         {
             Labirint();
+        }
+        static int height = 10;
+        static int width = 12;
+        static  int[,] LabirintLine = new int[height, width];
+        static int DogX = 1;
+        static int DogY = 1;
+        static int Block = 28;
+        static void Draw()
+        {
+             Console.Clear();
+             for (int i = 0; i < LabirintLine.GetLength(0); i++)
+             {
+             for (int j = 0; j < LabirintLine.GetLength(1); j++)
+             {
+                 if (LabirintLine[i, j] > Block) Console.Write(".");
+                 if (LabirintLine[i, j] < Block) Console.Write("#");
+                 if (LabirintLine[i, j] == Block) Console.Write("F");
+             }
+             Console.WriteLine();
+             }
+            DogPlace();
+        }
+        static void DogPlace()
+        {
+             Console.CursorLeft = DogX;
+             Console.CursorTop = DogY;
+             Console.Write("@");
+        }
+        static void GeneratField()
+        {
+             Random RandomSizeOfMap = new Random();
+             for (int Height = 0; Height < height; Height++)
+             {
+                 for (int Width = 0; Width < width; Width++)
+                 {
+                    LabirintLine[Height, Width] = RandomSizeOfMap.Next(0, 100);
+                 }
+
+            }
+        }
+        static void Logic()
+        {
+             ConsoleKeyInfo KeyDown = Console.ReadKey(true);
+             if (KeyDown.Key == ConsoleKey.LeftArrow && LabirintLine[DogY, DogX - 1] >= Block) DogX--;
+             if (KeyDown.Key == ConsoleKey.RightArrow && LabirintLine[DogY, DogX + 1] >= Block) DogX++;
+             if (KeyDown.Key == ConsoleKey.UpArrow && LabirintLine[DogY - 1, DogX] >= Block) DogY--;
+             if (KeyDown.Key == ConsoleKey.DownArrow && LabirintLine[DogY + 1, DogX] >= Block) DogY++;
+             if (LabirintLine[DogY, DogX] == Block)
+             {
+                  Console.WriteLine("Win");
+                  KeyDown = Console.ReadKey(false);
+                  if (KeyDown.Key == ConsoleKey.LeftArrow && LabirintLine[DogY, DogX - 1] >= Block) DogX = DogX;
+                  if (KeyDown.Key == ConsoleKey.RightArrow && LabirintLine[DogY, DogX + 1] >= Block) DogX = DogX;
+                  if (KeyDown.Key == ConsoleKey.UpArrow && LabirintLine[DogY - 1, DogX] >= Block) DogY = DogY;
+                  if (KeyDown.Key == ConsoleKey.DownArrow && LabirintLine[DogY + 1, DogX] >= Block) DogY = DogY;
+            }
 
         }
+        
+        
         static void Labirint()
         {
             Console.CursorVisible = false;
-            int[,] LabirintLine = new int[29,110];
-            Random RandomSizeOfMap = new Random();
-            for (int Height = 0; Height < 29; Height++)
-            {
-                for (int Width = 0; Width < 110; Width++)
-                {
-                    LabirintLine[Height, Width] = RandomSizeOfMap.Next(-1, 10);
-                }
-
-            }
-                
-                    
-
-
-            int x = 1;
-            int y = 1;
+            GeneratField();
             while (true)
             {
-                Console.Clear();
-                for (int i = 0; i < LabirintLine.GetLength(0); i++)
-                {
-                for (int i2 = 0; i2 < LabirintLine.GetLength(1); i2++)
-                {
-                    if (LabirintLine[i, i2] <= 8) Console.Write(" ");
-                    if (LabirintLine[i, i2] > 8) Console.Write("#");
-
-                }
-                Console.WriteLine();
-                }
-                Console.CursorLeft = x;
-                Console.CursorTop = y;
-                Console.Write("@");
-
-                ConsoleKeyInfo KeyDown = Console.ReadKey(true);
-                if (KeyDown.Key == ConsoleKey.Escape) break;
-                if (KeyDown.Key == ConsoleKey.LeftArrow && LabirintLine[y, x - 1] <= 8) x--;
-                if (KeyDown.Key == ConsoleKey.RightArrow && LabirintLine[y, x + 1] <= 8) x++;
-                if (KeyDown.Key == ConsoleKey.UpArrow && LabirintLine[y - 1, x] <= 8) y--;
-                if (KeyDown.Key == ConsoleKey.DownArrow && LabirintLine[y + 1, x] <= 8) y++;
+                Draw();
+                Logic();
             }
         }
         
